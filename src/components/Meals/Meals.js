a474b12ac8699fda9080df36e7a546611963c9b4
wckedwisdom0911 <shivamrawat091125@gmail.com>
2023-02-08 14:02:06 +0530

    files added

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+
+
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
